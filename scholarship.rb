11970e06721f55eef5cf433e6cb8698209866409
Phillip Gray <escapethegray11235@gmail.com>
2015-03-17 00:17:36 -0400

    initial commit

---
+++ scholarship.rb
@@ -0,0 +1,13 @@
+#ideas for scholarship submission
+
+=begin
+Include a menu for choosing options.
+* have an explicit/clean version exit commmand: gtfo vs i'm outta heah
+1) sentence translation based on hash
+2) pronunciation lookup
+	a) against hash table
+	b) attempt to translate by slicing string and looking for easily translatable chunks. e.g. er => ah, our/ore => oah
+
+*use a hash with punctuation and numbers to store and recall punctuation during translation
+*find a way to preserve capitalization => maybe using an array of string indexes?
+=end
